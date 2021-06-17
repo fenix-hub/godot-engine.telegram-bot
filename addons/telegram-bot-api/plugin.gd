2888cc312d4721c968ccb81c4c3974d3d78fb421
Nicolò Santilio <n.santilio97@gmail.com>
2021-06-17 20:42:02 +0200

    Dev (#1)
    * first commit
    
    * update README
    
    * Delete default_env.tres
    
    * update README
    
    * update

---
+++ addons/telegram-bot-api/plugin.gd
@@ -0,0 +1,9 @@
+tool
+extends EditorPlugin
+
+
+func _enter_tree() -> void:
+    add_autoload_singleton("TelegramAPI", "res://addons/telegram-bot-api/telegram-api/telegram_api.gd")
+
+func _exit_tree() -> void:
+    remove_autoload_singleton("TelegramAPI")
